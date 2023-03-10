8851c13818405660da45712887d7791873ed6e1c
Bartlomiej <16bartlomiejnowak@gmail.com>
2023-03-10 14:37:12 +0100

    Split scrape function into more parts

--- app/Services/Scraping/ScrapeService.php
+++ app/Services/Scraping/ScrapeService.php
@@ -4,8 +4,9 @@ namespace App\Services\Scraping;
 
 use GuzzleHttp\Client;
 
-class ScrapeService {
-    public function scrape() {
+class ScrapeService
+{
+    public function getDataFromURL() {
         // URL
         $url = 'https://pomagam.pl/t/popularne?current_page=1&type=category_projects';
 
@@ -28,26 +29,45 @@ class ScrapeService {
 
         // converting body to associative array
         $data = json_decode($body, true);
+
+        return [$data, $response_status_code];
+    }
+
+    public function findData($html) {
+        preg_match_all(config('constants.regex.title'), $html, $titles);
+        preg_match_all(config('constants.regex.slug'), $html, $slugs);
+        preg_match_all(config('constants.regex.image'), $html, $imgs);
+        preg_match_all(config('constants.regex.alt'), $html, $alts);
+        preg_match_all(config('constants.regex.number_id'), $html, $numbers_id);
+        preg_match_all(config('constants.regex.amount'), $html, $amounts);
+        for ($i = 0; $i < count($amounts); $i++) {
+            $amounts[$i] = preg_replace('/\xc2\xa0/', '', $amounts[$i]);
+            $amounts[$i] = str_replace(" ", "", $amounts[$i]);
+        }
+
+        foreach($amounts[0] as &$a) {
+            $a = (int)$a;
+        }
+        
+        return [$titles[0], $slugs[0], $imgs[0], $alts[0], $numbers_id[0], $amounts[0]];
+    }
+    
+    
+
+    public function scrape()
+    {
+        list($data, $response_status_code) = $this->getDataFromURL();
+
         $html = $data['html'];
         $has_next = $data['has_next'];
 
-        
         if ($response_status_code == 200) {
-            preg_match_all(config('constants.regex.title'), $html, $title);
-            preg_match_all(config('constants.regex.slug'), $html, $slug);
-            preg_match_all(config('constants.regex.image'), $html, $img);
-            preg_match_all(config('constants.regex.alt'), $html, $alt);
-            preg_match_all(config('constants.regex.number_id'), $html, $number_id);
-            preg_match_all(config('constants.regex.amount'), $html, $amount);
-            for($i = 0; $i < count($amount); $i++) {
-                $amount[$i] = preg_replace('/\xc2\xa0/', '', $amount[$i]);
-                $amount[$i] = str_replace(" ", "", $amount[$i]);
-            }
-
-            dd($title, $slug, $img, $alt, $number_id, $amount);
-            dd($html, $has_next);
+            list($titles, $slugs, $imgs, $alts, $numbers_id, $amounts) = $this->findData($html);
         }
-
-        dd($response);
+            
+        dd($titles, $slugs, $imgs, $alts, $numbers_id, $amounts);
+        dd($html, $has_next);
     }
+
+    
 }
