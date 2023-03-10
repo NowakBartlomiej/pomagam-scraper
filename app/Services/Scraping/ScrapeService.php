06cc02d46754c72354fe81685aaf5d92fb12d1a0
Bartlomiej <16bartlomiejnowak@gmail.com>
2023-03-10 16:52:42 +0100

    Implement a method that searches all popular collections and insert or update this data to database

--- app/Services/Scraping/ScrapeService.php
+++ app/Services/Scraping/ScrapeService.php
@@ -2,14 +2,15 @@
 
 namespace App\Services\Scraping;
 
+use App\Models\Data;
 use GuzzleHttp\Client;
+use Illuminate\Support\Arr;
 
 class ScrapeService
 {
-    public function getDataFromURL() {
-        // URL
-        $url = 'https://pomagam.pl/t/popularne?current_page=1&type=category_projects';
 
+    public function getDataFromURL($url)
+    {
         // Initial guzzle
         $client = new Client();
 
@@ -33,7 +34,53 @@ class ScrapeService
         return [$data, $response_status_code];
     }
 
-    public function findData($html) {
+    public function scrape()
+    {
+        $pageNumber = 1;
+        // URL
+        $url = "https://pomagam.pl/t/popularne?current_page=$pageNumber&type=category_projects";
+        
+        // Getting data from URL and resposnse status code
+        list($data, $response_status_code) = $this->getDataFromURL($url);
+        
+        // is website has more data
+        $has_next = $data['has_next'];
+
+        while ($has_next) {
+            // URL
+            $url = "https://pomagam.pl/t/popularne?current_page=$pageNumber&type=category_projects";    
+
+            // Getting data from URL and resposnse status code
+            list($data, $response_status_code) = $this->getDataFromURL($url);
+
+            // HTML of current url
+            $html = $data['html'];
+            
+
+            // Getting finded datas from html
+            list($titles, $slugs, $imgs, $alts, $numbers_id, $amounts) = $this->findData($html);
+
+            // Making array with all datas
+            // $values = array(
+            //     $titles, $slugs, $imgs, $alts, $numbers_id, $amounts
+            // );
+
+            //! Tu dodawac do bazy dancyh
+            echo $pageNumber . "\xA";
+            for ($i = 0; $i < count($titles); $i++) {
+                Data::upsert([
+                    'title' => $titles[$i], 'slug' => $slugs[$i], 'image' => $imgs[$i], 'alt' => $alts[$i], 'number_id' => $numbers_id[$i], 'amount' => $amounts[$i]
+                ], ['number_id'], ['amount']);
+            }
+           
+            $pageNumber = $pageNumber + 1;
+            // is website has more data
+            $has_next = $data['has_next'];
+        }
+    }
+
+    public function findData($html)
+    {
         preg_match_all(config('constants.regex.title'), $html, $titles);
         preg_match_all(config('constants.regex.slug'), $html, $slugs);
         preg_match_all(config('constants.regex.image'), $html, $imgs);
@@ -45,29 +92,10 @@ class ScrapeService
             $amounts[$i] = str_replace(" ", "", $amounts[$i]);
         }
 
-        foreach($amounts[0] as &$a) {
+        foreach ($amounts[0] as &$a) {
             $a = (int)$a;
         }
-        
+
         return [$titles[0], $slugs[0], $imgs[0], $alts[0], $numbers_id[0], $amounts[0]];
     }
-    
-    
-
-    public function scrape()
-    {
-        list($data, $response_status_code) = $this->getDataFromURL();
-
-        $html = $data['html'];
-        $has_next = $data['has_next'];
-
-        if ($response_status_code == 200) {
-            list($titles, $slugs, $imgs, $alts, $numbers_id, $amounts) = $this->findData($html);
-        }
-            
-        dd($titles, $slugs, $imgs, $alts, $numbers_id, $amounts);
-        dd($html, $has_next);
-    }
-
-    
 }
